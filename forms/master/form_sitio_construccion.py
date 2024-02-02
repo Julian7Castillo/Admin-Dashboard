ac8552ed04125e87e4b8c2ab56ee049b94623258
Julian Castillo <oscarcastillo212002@gmail.com>
2024-02-01 23:49:57 -0500

    Creacion de panel para las páginas en construcción

--- forms/master/form_sitio_construccion.py
+++ forms/master/form_sitio_construccion.py
@@ -3,5 +3,17 @@ from config import COLOR_CUERPO_PRINCIPAL
 
 class FormularioSitioConstruccionDesign():
     
-    def __init__(self, panel_priuncipal, logo):
-        pass
+    def __init__(self, panel_principal, logo):
+        self.barra_superior = tk.Frame(panel_principal)
+        self.barra_superior.pack(side=tk.TOP, fill= tk.X, expand=False)
+        
+        self.barra_inferior = tk.Frame(panel_principal)
+        self.barra_inferior.pack(side=tk.BOTTOM, fill="both", expand=True)
+        
+        self.labelTitulo = tk.Label(self.barra_superior, text = " Pagina en cosntrucción")
+        self.labelTitulo.config(fg = "#222d33", font=("Roboto", 30), bg=COLOR_CUERPO_PRINCIPAL)
+        self.labelTitulo.pack(side=tk.TOP, fill="both", expand=True)
+        
+        self.label_Imagebn = tk.Label(self.barra_inferior, image=logo)
+        self.label_Imagebn.place(x = 0, y = 0, relwidth=1, relheight=1)
+        self.label_Imagebn.config(fg = "#fff", font=("Roboto", 10), bg=COLOR_CUERPO_PRINCIPAL)
